c2f68122c79a15e9d9f14042298283705831b6ce
Josh Klein <skiz303@gmail.com>
2022-09-01 20:39:36 -0400

    fixed spacing on team page

--- page-team.php
+++ page-team.php
@@ -2,16 +2,18 @@
 
 <main>
     <h2>Meet our team!</h2>
-    <?php
-    $team_members = get_field('team');
+    <div class="team-members">
+        <?php
+        $team_members = get_field('team');
 
-    foreach ($team_members as $team_member) { ?>
-        <div class="team-member">
-            <h3><?php echo ($team_member['team_member_name']); ?></h3>
-            <img src="<?php echo wp_get_attachment_url($team_member['team_member_photo']); ?>" class="team-member-photo">
-            <p><?php echo ($team_member['team_member_bio']); ?></p>
-        </div>
-    <?php } ?>
+        foreach ($team_members as $team_member) { ?>
+            <div class="team-member">
+                <h3><?php echo ($team_member['team_member_name']); ?></h3>
+                <img src="<?php echo wp_get_attachment_url($team_member['team_member_photo']); ?>" class="team-member-photo">
+                <p><?php echo ($team_member['team_member_bio']); ?></p>
+            </div>
+        <?php } ?>
+    </div>
 </main>
 
 <?php get_footer(); ?>
